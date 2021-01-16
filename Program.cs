87406f4d001d06778c74b8d90a30172658c99e01
Stefanos Bekiaris <sbekiaris@switchautomation.com>
2021-01-16 16:01:36 +0000

    Added gitignore

--- Program.cs
+++ Program.cs
@@ -7,42 +7,46 @@ namespace ai_from_scratch
 {
     class Program
     {
-        private static double[] inputs = new[] {1.0, 2.0, 3.0, 2.5};
-
-        // Each neuron has its own weight and bias
-        private static double[][] weights = new[] {
-            new double[] { 0.2, 0.8, -0.5, 1 },
-            new double[] { .5, -0.91, 0.26, -0.5 },
-            new double[] { -0.26, -0.27, 0.17, 0.87 }
+        private static double[][] inputs = new[]
+        {
+            new[] {1.0, 2.0, 3.0, 2.5},
+            new[] {2.0, 5.0, -1.0, 2.0},
+            new[] {-1.5, 2.7, 3.3, -0.8}
         };
+        // Each neuron has its own weight and bias
+        private static double[][] weights = new[] 
+        {
+            new[] { 0.2, 0.8, -0.5, 1.0 },
+            new[] { 0.5, -0.91, 0.26, -0.5 },
+            new[] { -0.26, -0.27, 0.17, 0.87 }
+        };
+        
         private static double[] biases = new[] {2.0, 3.0, 0.5};
 
         static void Main(string[] args)
         {
-            List<double> layerOutputs = new List<double>();
-            var weighted_output = Matrix.Dot(weights, inputs);
-            foreach (var (neuronOutput, bias) in weighted_output.Zip(biases))
+            double[][] weighted_output = {};
+            if (inputs.GetType() != typeof(double[]))
             {
-                layerOutputs.Add(neuronOutput + bias);
+                weighted_output = Matrix.Dot( inputs, weights.Transpose())
+                    .Select(val=> val.Select((s, i) => s + biases[i]).ToArray())
+                    .ToArray();
             }
-            
-            // foreach (var (neuronWeight, neuronBias) in weights.Zip<double[], double>(biases))
-            // {
-            //     var neuronOutput = 0.0;
-            //     foreach (var (nInput, weight) in inputs.Zip<double, double>(neuronWeight)){
-            //         neuronOutput += nInput*weight;
-            //     }
-            //     
-            //     neuronOutput += neuronBias;
-            //     layerOutputs.Add(neuronOutput);
-            // }
-
-            foreach (var item in layerOutputs)
+            else
             {
-                Console.Write($"{item.ToString()}, ");    
+                weighted_output = Matrix.Dot( inputs, weights)
+                    .Select(val=> val.Select((s, i) => s + biases[i]).ToArray())
+                    .ToArray();
             }
 
-            Console.Read();
+            foreach (var items in weighted_output)
+            {
+                foreach (var item in items)
+                {
+                    Console.Write($"{item.ToString()}, ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
